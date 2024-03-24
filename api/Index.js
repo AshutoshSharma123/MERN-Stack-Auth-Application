363f7a026b10e0cc40b0e43a7052f4fc74391b9f
AshutoshSharma123 <aashusharma375@gmail.com>
2024-03-24 12:02:56 +0530

    connected to mongodb

--- api/Index.js
+++ api/Index.js
@@ -1,8 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
+
+// mongoose.connect('mongodb+srv://aashusharma375:ashutosh@authentication.vol8rrr.mongodb.net/?retryWrites=true&w=majority&appName=authentication').then(() => {
+//     console.log('connected to mongodb');
+// }).catch((err) => {
+//     console.log(err);
+// });
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to mongodb');
+}).catch((err) => {
+    console.log(err);
+});
 
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
-})
+});
