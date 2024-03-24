a11e1726364e3dc59c75a7fb4bbcd058eca578fc
AshutoshSharma123 <aashusharma375@gmail.com>
2024-03-24 10:16:18 +0530

    setup and created server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className='bg-slate-200 ' >
+        <div className='bg-slate-200 h-9 ' >
             <div className="flex justify-between items-center mx-auto max-w-6xl ">
                 <Link to='/'>    <h1 className='font-bold '>Auth Application</h1></Link>
 
-                <ul className='flex gap-4'>
-                    <Link to='/'><li>HOME</li></Link>
-                    <Link to='/about'><li>ABOUT</li></Link>
-                    <Link to='/sign-in'><li>SIGN-IN</li></Link>
+                <ul className='flex gap-4 font-semibold'>
+                    <Link to='/'><li>Home</li></Link>
+                    <Link to='/about'><li>About</li></Link>
+                    <Link to='/sign-in'><li>Sign-in</li></Link>
 
 
 
